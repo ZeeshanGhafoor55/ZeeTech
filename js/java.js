e7853a0b1541e145899b60b884f53a4c9bff4cd1
Zeeshan-Waince <sobanweb98@gmail.com>
2022-10-20 21:18:35 -0700

    First commit

---
+++ js/java.js
@@ -0,0 +1,41 @@
+$(window).scroll(function() {    
+    var scroll = $(window).scrollTop();    
+    if (scroll > 120) {
+        $(".navbar").addClass("fixed-top");
+    }
+    else {
+        $(".navbar").removeClass("fixed-top");
+    }
+});
+
+
+//portfolio filter
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
